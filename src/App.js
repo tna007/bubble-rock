1429311e6ea849526618859a9450504748f4732c
tna007 <anh.to@edu.bc.fi>
2021-11-25 12:53:51 +0200

    Link components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Cart from "./components/Cart";
+
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import ProductList from "./containers/ProductList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={ProductList} />
+      </Switch>
+      <p></p>
+      <div>here will be cart</div>
+      <Cart />
+    </Router>
   );
 }
 
